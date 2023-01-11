271fc00aa2b2ac3056be8e5151644e3cb8472a9e
Davide Sanvito <dsanvito90@gmail.com>
2023-01-11 17:34:22 +0100

    Added MIDAS & AD_aggregator K8s scripts (2)

--- k8s/ad_aggregator_k8s.sh
+++ k8s/ad_aggregator_k8s.sh
@@ -3,9 +3,6 @@
 BASE_DIR="/media/palantir-nfs/ti-mmml-ad"
 AD_aggregator_DIR="${BASE_DIR}/k8s/AD_aggregator/ad_aggr_docker"
 
-echo "Refreshing code"
-cd ${BASE_DIR} && git pull origin master
-
 echo "Rebuilding AD_aggregator docker image..."
 cd ${AD_aggregator_DIR} && docker build -t palantir-ad-aggregator:1.0 . && docker tag palantir-ad-aggregator:1.0 10.101.10.244:5000/palantir-ad-aggregator:1.0 && docker push 10.101.10.244:5000/palantir-ad-aggregator:1.0
 if [[ $(kubectl get pods --all-namespaces | grep ad-aggregator | wc -l) -gt 0 ]]; then
